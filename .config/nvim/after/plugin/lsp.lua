17fd39d184f11769b2c8e37210303a378c7da184
Vittorio Bertoli <vitt@vittoriobertoli.xyz>
2024-06-29 11:21:55 +1000

    Ruby lsp.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,6 @@
 require('lsp-zero')
-require('lspconfig').tsserver.setup({})
 -- Install servers via $ npm i -g vscode-langservers-extracted
 require'lspconfig'.html.setup{}
 require'lspconfig'.cssls.setup{}
+require'lspconfig'.ruby_lsp.setup{"ruby-lsp"}
+--require('lspconfig').tsserver.setup({})
